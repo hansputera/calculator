7f144d91902387adc5313605a67c182cb68ff043
hansputera <47862061+hansputera@users.noreply.github.com>
2020-04-30 18:37:34 +0700

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -5,9 +5,16 @@ alert(`Angka yang ingin di eksekusi tidak ada tanda [+,-,*,/] !`);
 return true;
 } else {
 let result = eval(data);
-
 alert(`Hasil Eksekusi: ${result}`);
 data = '';
 return true;
 };
 };
+
+function changeTitle() {
+let array = ['Simple Calculator', 'Calculator Tambah²', 'Kalkulator:v'];
+let rand = Math.floor(Math.random() * array.length);
+document.title = array[rand];
+};
+
+setInterval(changeTitle, 10 * 1000);
